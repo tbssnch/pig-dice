7fc277d9623fe061c652c746c7901aa0267a0a87
Jenn Trass and Tyler Bussanich <jenn@>
2017-11-02 14:33:41 -0700

    add working function that counts player one score and resets to zero when a one is rolled

--- js/scripts.js
+++ js/scripts.js
@@ -1,63 +1,46 @@
 //back-end logic
-function Player (active, score) {
-  this.active=active;
-  this.score =score;
+function Player (activePlayer, turnScore, gameScore) {
+  this.activePlayer= activePlayer;
+  this.turnScore = turnScore;
+  this.gameScore = gameScore;
 }
 
-
-// var rollScore= 0;
-
-Player.prototype.totalScore = function (randomNumber) {
-  var turnArray=[];
-  function add (a,b) {
-    return a + b;
-  }
-  if (randomNumber === 1) {
-   var rollScore = 0;
- } else {
-   var rollScore= randomNumber;
-   turnArray.push(rollScore);
-   this.score += turnArray.reduce(add, 0);
- }
+var turnScoreArray=[];
+function add (a,b) {
+  return a+b;
 }
 
+Player.prototype.newTurnScore = function (roll) {
+  if (roll === 1) {
+    this.turnScore = 0;
+    return this.turnScore;
+  } else {
+  turnScoreArray.push(roll);
 
-//user-interface logic
-$(document).ready(function () {
-  var playerOne = new Player (true, 0);
-  var playerTwo = new Player (false, 0);
-  $(".roll").click(function () {
+  var turnScoreTotal = turnScoreArray.reduce(add, 0);
+  this.turnScore = turnScoreTotal;
+  return this.turnScore;
+}
+
+};
+
+
+
+
+// user-interface logic
+$(document).ready(function() {
+  $(".roll").click(function() {
+    var playerOne = new Player (true,0,0);
     event.preventDefault();
+debugger;
+    var rollScore = Math.floor(Math.random() * 6) + 1;
 
-    debugger;
-    var random = Math.floor(Math.random() * 6) + 1;
+    var currentScore = playerOne.newTurnScore(rollScore);
 
-    alert (playerOne.totalScore(random));
-
-
-
-
-
-    // if (random === 1) {
-    //   var rollScore=0;
-    // } else if (random >= 2) {
-    //  var rollScore = random;
-    //  userOneArray.push(rollScore);
-    // }
-
-  //
-  // $(".hold").click(function() {
-  //   event.preventDefault();
-  //
-  //
-  //
-  //   // var rollArray=[];
-  //   // rollArray.push(newRoll.rollValue);
-
-
-    // $(".thisRoll").text(rollScore);
-
-
+    $(".playerOneRollScore").text(rollScore);
+    $(".playerOneTurnScore").text(currentScore);
+    $(".playerTwoRollScore").text(rollScore);
+    $(".playerTwoTurnScore").text(currentScore);
 
   });
 });
